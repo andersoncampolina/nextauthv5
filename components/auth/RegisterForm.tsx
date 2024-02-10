05602cda5fff6c4a89cb775fafcac9162fee5784
Anderson Campolina <andersoncampolina@Andersons-MacBook-Pro.local>
2024-02-09 23:10:42 -0300

    Register Form - Completado

---
+++ components/auth/RegisterForm.tsx
@@ -0,0 +1,122 @@
+'use client'
+import React from 'react'
+import { CardWrapper } from '@/components/auth/CardWrapper'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { RegisterSchema } from '@/schemas'
+import { z } from 'zod';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import FormError from '../FormError';
+import FormSuccess from '../FormSuccess';
+import { login } from '@/actions/login';
+import { useTransition } from 'react';
+
+export function RegisterForm() {
+
+    const [isPending, startTransition] = useTransition();
+    const [error, setError] = React.useState<string | undefined>('')
+    const [success, setSuccess] = React.useState<string | undefined>('')
+
+    const form = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+            name: '',
+        }
+    })
+
+    const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+
+        setError(undefined);
+        setSuccess(undefined);
+
+        startTransition(() => {
+            login(values).then((res) => {
+                setError(res.error);
+                setSuccess(res.success);
+            })
+        })
+    }
+
+    return (
+        <div>
+            <CardWrapper 
+                headerLabel='Criar uma conta'
+                backButtonLabel='Já possui uma conta?'
+                backButtonHref='/auth/login'
+                showSocial
+            >
+                <Form {...form}>
+                    <form 
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className='space-y-6'
+                    >
+                        <div className='space-y-4'>
+                            <FormField 
+                                control={form.control}
+                                name='name'
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Nome</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                disabled={isPending}
+                                                placeholder='Fulano de Tal'
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField 
+                                control={form.control}
+                                name='email'
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                disabled={isPending}
+                                                placeholder='seu.email@exemplo.com'
+                                                type='email'
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField 
+                                control={form.control}
+                                name='password'
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                {...field}
+                                                disabled={isPending}
+                                                placeholder='******'
+                                                type='password'
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <Button disabled={isPending} type='submit' className='w-full'>
+                            Login
+                        </Button>
+                        <FormError message={error}/>
+                        <FormSuccess message={success}/>
+                    </form>
+                </Form>
+            </CardWrapper>
+        </div>
+    )
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,14 @@ export const LoginSchema = z.object({
     }),
     password: z.string().min(1, {message: 'Preencha o password.'}),
 })
+
+export const RegisterSchema = z.object({
+    email: z.string({
+        invalid_type_error: 'O email deve ser uma string.',
+    
+    }).email({
+        message: 'Preencha o email.',
+    }),
+    password: z.string().min(6 , {message: 'O password deve ter no mínimo 6 caracteres.'}),
+    name: z.string().min(1, {message: 'Preencha o nome.'}),
+})
